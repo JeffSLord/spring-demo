047174d8c19217a4b2bcaeefba44b3bd76a9a39f
Jeff <jeffscottlord@gmail.com>
2021-01-19 14:04:50 -0500

    Controller vs RestController
    RestController sets body as well for convenience. Controller for returning view template.

--- src/main/java/com/jeffslord/springdemo/SpringDemoApplication.java
+++ src/main/java/com/jeffslord/springdemo/SpringDemoApplication.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+//@RestController
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String hello(){
-		return "Hello, World";
-	}
+//	@GetMapping("/")
+//	public String hello(){
+//		return "Hello, World";
+//	}
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name){
